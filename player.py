6eb5bdb32456eead5b9c80b3e9a3f8051a023d19
mayuda10 <0600011922@sinodal.com.br>
2026-07-09 12:02:23 -0300

    Add Player class with attributes and display method

---
+++ player.py
@@ -0,0 +1,15 @@
+class Player:
+
+    def __init__(self):
+
+        self.nome = "Herói"
+
+        self.vida = 100
+
+        self.moedas = 0
+
+    def mostrar(self):
+
+        print("Nome:", self.nome)
+        print("Vida:", self.vida)
+        print("Moedas:", self.moedas)
